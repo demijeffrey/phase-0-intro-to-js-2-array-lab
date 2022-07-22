60a580273bf937b4225266063519a5d669854599
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-21 22:06:53 -0600

    commit tree ad2994c2ee99626ab5483c780ac44ae0c0f1f047

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat () {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift()
+}
+
+function appendCat () {
+    const copyCats = [...cats, 'Broom']
+    return copyCats
+}
+
+function prependCat () {
+    const copyCats = ["Arnold", ...cats]
+    return copyCats
+}
+
+function removeLastCat () {
+    const copyCats = cats.slice(0, cats.length - 1)
+    return copyCats
+}
+
+function removeFirstCat () {
+    const copyCats = cats.slice(1)
+    return copyCats
+}
